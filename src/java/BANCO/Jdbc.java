425d753157720bc7567b9d344bf52a262c93f97f
catie <catie@LAPTOP-G2A8BQKA>
2018-10-12 22:28:52 -0300

    CONEXÃO JDBC

---
+++ src/java/BANCO/Jdbc.java
@@ -0,0 +1,31 @@
+package BANCO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Jdbc {
+
+
+    private static Connection con = null;
+
+
+    public Connection criarconexcao() throws SQLException, ClassNotFoundException {
+      
+ 
+        String dbName = "teste";
+        String userName = "root";
+        String password = "erick5567";
+        String hostname = "localhost";
+        String port = "3306";
+        String jdbcUrl = "jdbc:mysql://" + hostname + ":"
+                + port + "/" + dbName + "?user=" + userName + "&password=" + password+"&autoReconnect=true";
+        Class.forName("com.mysql.jdbc.Driver");
+        con = DriverManager.getConnection(jdbcUrl);
+        
+        return con;
+    }
+    
+    
+    
+    
+}
