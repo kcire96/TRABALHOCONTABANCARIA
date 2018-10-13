a0ba139ad6cb2994e4dd03a8c3e8fc7244ea5f4e
catie <catie@LAPTOP-G2A8BQKA>
2018-10-12 22:26:48 -0300

    CLASSE CADASTRO DE CONTA

---
+++ src/java/classes/conta.java
@@ -0,0 +1,39 @@
+package classes;
+
+
+public class conta {
+    
+    private int id_conta;
+    private String numero_conta;
+    private String cpf_titular;
+
+  
+    public int getId_conta() {
+        return id_conta;
+    }
+
+
+    public void setId_conta(int id_conta) {
+        this.id_conta = id_conta;
+    }
+
+  
+    public String getNumero_conta() {
+        return numero_conta;
+    }
+
+    public void setNumero_conta(String numero_conta) {
+        this.numero_conta = numero_conta;
+    }
+
+
+    public String getCpf_titular() {
+        return cpf_titular;
+    }
+
+  
+    public void setCpf_titular(String cpf_titular) {
+        this.cpf_titular = cpf_titular;
+    }
+    
+}
